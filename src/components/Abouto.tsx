738707725c855f0cd9fa9c58e35b544534055ce3
little barra 👍 <gustavobarrinha.dev2024@gmail.com>
2025-04-29 15:18:40 -0300

    Update Abouto.tsx

--- src/components/Abouto.tsx
+++ src/components/Abouto.tsx
@@ -17,7 +17,7 @@ const Abouto = () => {
       id="about"
       className="min-h-screen flex flex-col justify-center items-center px-4 relative overflow-hidden bg-gray-900"
     >
-      {/* Código animado lateral direita */}
+      
       <div className="absolute left-0 top-[10%] w-full z-0">
         <pre className="animate-code-right text-green-500 text-2xl font-mono opacity-70">
 {`const sobre = () => {
@@ -26,7 +26,7 @@ const Abouto = () => {
         </pre>
       </div>
 
-      {/* Código animado lateral esquerda */}
+    
       <div className="absolute left-0 bottom-[10%] w-full z-0">
         <pre className="animate-code-left text-green-500 text-2xl font-mono opacity-70">
 {`const sobre = () => {
@@ -41,14 +41,12 @@ const Abouto = () => {
         transition={{ duration: 0.8 }}
         className="max-w-6xl w-full mx-auto relative z-10 flex flex-col md:flex-row items-center justify-center gap-12 text-left py-12"
       >
-        {/* Imagem centralizada e maior */}
+        
         <img
           src="public\WhatsApp Image 2025-04-29 at 12.39.20.jpeg"
           alt="Foto de Gustavo Barrinha"
           className="w-64 h-64 md:w-72 md:h-72 rounded-full object-cover border-4 border-green-400 shadow-2xl"
         />
-
-        {/* Texto */}
         <div className="text-center md:text-left space-y-6 max-w-xl">
           <h2 className="text-4xl md:text-6xl font-[Orbitron] bg-clip-text text-transparent bg-gradient-to-r from-green-500 via-emerald-500 to-lime-400">
             Quem Sou Eu
